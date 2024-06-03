954e208ff27603bd77155585ec0332fec2d996cc
piro0919 <piro.haniwa@gmail.com>
2024-06-03 22:04:34 +0900

    feat: add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  basePath: "/hoge",
+});
 
 export default nextConfig;
